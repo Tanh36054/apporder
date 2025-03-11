af235efff0efe47c16d710dfbec1dddb2959fda1
Anhdz <tanh36054@gmail.com>
2025-03-11 10:31:07 +0700

    add food success

--- lib/admin/add_food.dart
+++ lib/admin/add_food.dart
@@ -1,10 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
-
-import 'package:apporder/widget/widget_support.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/gestures.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
+import '../widget/widget_support.dart';
 
 class AddFood extends StatefulWidget {
   const AddFood({super.key});
@@ -16,31 +16,80 @@ class AddFood extends StatefulWidget {
 class _AddFoodState extends State<AddFood> {
   final List<String> items = ['Ice-cream', 'Burger', 'Salad', 'Pizza'];
   String? value;
-  TextEditingController namecontroller = new TextEditingController();
-  TextEditingController pricecontroller = new TextEditingController();
-  TextEditingController detailcontroller = new TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController detailController = TextEditingController();
   final ImagePicker _picker = ImagePicker();
   File? selectedImage;
+  String? base64Image;
 
-  Future getImage() async {
-    var image = await _picker.pickImage(source: ImageSource.gallery);
-
-    selectedImage = File(image!.path);
-    setState(() {});
+  // Hàm chọn ảnh từ thư viện
+  Future<void> pickImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      File file = File(image.path);
+      List<int> imageBytes = await file.readAsBytes();
+      setState(() {
+        selectedImage = file;
+        base64Image = base64Encode(imageBytes);
+      });
+    }
   }
 
-  uploadItem() {
-    if (selectedImage != null &&
-        namecontroller.text != "" &&
-        pricecontroller != "" &&
-        detailcontroller != "") {
-      // String addId = randomAlphaNumeric(10);
+  // Upload ảnh lên Imgur
+  Future<String?> uploadToImgur(String base64Image) async {
+    const String clientId =
+        "a7e67cb02e6bef3"; // Thay YOUR_CLIENT_ID bằng ID của bạn
+    var response = await http.post(
+      Uri.parse("https://api.imgur.com/3/image"),
+      headers: {"Authorization": "Client-ID $clientId"},
+      body: {"image": base64Image, "type": "base64"},
+    );
 
-      // Reference firebaseStorageRef = FirebaseStorage.instance
-      //     .ref()
-      //     .child("blogImages")
-      //     .child(addId);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data["data"]["link"]; // Trả về link ảnh
+    } else {
+      debugPrint("Error upload img: ${response.body}");
+      return null;
     }
+  }
+
+  // Upload dữ liệu lên Firestore
+  Future<void> uploadItem() async {
+    if (base64Image == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Please select img")));
+      return;
+    }
+
+    String? imageUrl = await uploadToImgur(base64Image!);
+    if (imageUrl == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Error upload img Imgur")));
+      return;
+    }
+
+    if (value == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Please select a category")));
+      return;
+    }
+
+    await FirebaseFirestore.instance.collection(value!).add({
+      "image": imageUrl,
+      "name": nameController.text,
+      "price": double.tryParse(priceController.text),
+      "detail": detailController.text,
+      "category": value, // Lưu category đã chọn
+    });
+
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text("Add Successfull!")));
   }
 
   @override
@@ -48,20 +97,18 @@ class _AddFoodState extends State<AddFood> {
     return Scaffold(
       appBar: AppBar(
         leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
+          onTap: () => Navigator.pop(context),
           child: Icon(
             Icons.arrow_back_ios_new_outlined,
             color: Color(0xFF373866),
           ),
         ),
         centerTitle: true,
-        title: Text("Add Item", style: AppWidget.HeadLineTextFeildStyle()),
+        title: Text("Add Food", style: AppWidget.HeadLineTextFeildStyle()),
       ),
       body: SingleChildScrollView(
         child: Container(
-          margin: EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 30),
+          margin: EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -70,174 +117,136 @@ class _AddFoodState extends State<AddFood> {
                 style: AppWidget.semiBoldTextFeildStyle(),
               ),
               SizedBox(height: 20),
-              selectedImage == null
-                  ? GestureDetector(
-                    onTap: () {
-                      getImage();
-                    },
-                    child: Center(
-                      child: Material(
-                        elevation: 4,
+              Center(
+                child: GestureDetector(
+                  onTap: pickImage, // Gọi hàm chọn ảnh
+                  child: Material(
+                    elevation: 4,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 1.5),
                         borderRadius: BorderRadius.circular(20),
-                        child: Container(
-                          width: 150,
-                          height: 150,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 1.5),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Icon(
-                            Icons.camera_alt_outlined,
-                            color: Colors.black,
-                          ),
-                        ),
                       ),
-                    ),
-                  )
-                  : Center(
-                    child: Material(
-                      elevation: 4,
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        width: 150,
-                        height: 150,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black, width: 1.5),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.file(selectedImage!, fit: BoxFit.cover),
-                        ),
-                      ),
+                      child:
+                          selectedImage == null
+                              ? Icon(Icons.camera_alt_outlined, size: 50)
+                              : ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Image.file(
+                                  selectedImage!,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
                     ),
                   ),
+                ),
+              ),
               SizedBox(height: 30),
               Text("Item Name", style: AppWidget.semiBoldTextFeildStyle()),
               SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Color(0xFFececf8),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextField(
-                  controller: namecontroller,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Enter Item Name",
-                    hintStyle: AppWidget.LightTextFeildStyle(),
-                  ),
-                ),
-              ),
+              buildTextField(nameController, "Enter Item Name"),
+
               SizedBox(height: 30),
               Text("Item Price", style: AppWidget.semiBoldTextFeildStyle()),
               SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Color(0xFFececf8),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextField(
-                  controller: pricecontroller,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Enter Item Price",
-                    hintStyle: AppWidget.LightTextFeildStyle(),
-                  ),
-                ),
-              ),
+              buildTextField(priceController, "Enter Item Price"),
+
               SizedBox(height: 30),
-              Text("Item Detail", style: AppWidget.semiBoldTextFeildStyle()),
+              Text("Description", style: AppWidget.semiBoldTextFeildStyle()),
               SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Color(0xFFececf8),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextField(
-                  maxLines: 6,
-                  controller: detailcontroller,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Enter Item Detail",
-                    hintStyle: AppWidget.LightTextFeildStyle(),
-                  ),
-                ),
+              buildTextField(
+                detailController,
+                "Enter Description",
+                maxLines: 6,
               ),
+
               SizedBox(height: 20),
-              Text(
-                "Select Category",
-                style: AppWidget.semiBoldTextFeildStyle(),
-              ),
+              Text("Select Item", style: AppWidget.semiBoldTextFeildStyle()),
               SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Color(0xFFececf8),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    items:
-                        items
-                            .map(
-                              (item) => DropdownMenuItem<String>(
-                                value: item,
-                                child: Text(
-                                  item,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ),
-                            )
-                            .toList(),
-                    onChanged:
-                        ((value) => setState(() {
-                          this.value = value;
-                        })),
-                    dropdownColor: Colors.white,
-                    hint: Text("Select Category"),
-                    iconSize: 36,
-                    icon: Icon(Icons.arrow_drop_down, color: Colors.black),
-                    value: value,
-                  ),
-                ),
-              ),
+              buildDropdown(),
+
               SizedBox(height: 30),
-              Center(
-                child: Material(
-                  elevation: 5,
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Add",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+              GestureDetector(onTap: uploadItem, child: buildButton("Add")),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(
+    TextEditingController controller,
+    String hint, {
+    int maxLines = 1,
+  }) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Color(0xFFececf8),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        controller: controller,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hint,
+          hintStyle: AppWidget.LightTextFeildStyle(),
+        ),
+      ),
+    );
+  }
+
+  Widget buildDropdown() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Color(0xFFececf8),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          items:
+              items
+                  .map(
+                    (item) => DropdownMenuItem(value: item, child: Text(item)),
+                  )
+                  .toList(),
+          onChanged: (value) => setState(() => this.value = value),
+          dropdownColor: Colors.white,
+          hint: Text("Select Item"),
+          value: value,
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(String text) {
+    return Center(
+      child: Material(
+        elevation: 5,
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          width: 150,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
       ),
--- lib/service/database.dart
+++ lib/service/database.dart
@@ -14,6 +14,10 @@ class DatabaseMethods {
     });
   }
 
+  Future addFoodItem(Map<String, dynamic> userInfoMap, String name) async {
+    return await FirebaseFirestore.instance.collection(name).add(userInfoMap);
+  }
+
   // Future<Stream<QuerySnapshot>> getFoodItem(String name) async {
   //   return await firebaseFirestore.instance.collection(name).snapshot();
   // }
